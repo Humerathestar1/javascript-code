8d5fc902ed81648131a631a7d14c9b0db8f1a3e7
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-26 09:35:41 +0530

    Create count even odd.js

---
+++ count even odd.js
@@ -0,0 +1,19 @@
+let number = 20;
+let evencount =0;
+let oddcount=0;
+for(let i=1; i < number ;i++){
+    
+    if(i%2==0){
+     
+       evencount+=1;
+       
+    }
+    else{
+      
+       oddcount+=1;
+    }
+}
+  console.log("even",evencount);
+   console.log("odd",oddcount); 
+
+
